cc72c15e5adae2189686929662a44417f9f83d46
Sid Swaminathan <sidswaminathan@yahoo.com>
2024-06-11 11:40:17 -0500

    add #unpopular_items + #potential_revenue instance methods testing specs

--- lib/auction.rb
+++ lib/auction.rb
@@ -12,4 +12,12 @@ class Auction
     def item_names
         @items.map(&:name)
     end
+
+    def unpopular_items
+        @items.select { |item| item.bids.empty? }
+    end
+
+    def potential_revenue
+        @items.map { |item| item.bids.values.max || 0 }.sum
+    end
 end
--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -5,7 +5,12 @@ RSpec.describe Auction do
         @auction = Auction.new
         @item1 = Item.new('Chalkware Piggy Bank')
         @item2 = Item.new('Bamboo Picture Frame')
-        @attendee = Attendee.new({name: 'Megan', budget: '$50'})
+        @item3 = Item.new('Homemade Chocolate Chip Cookies')
+        @item4 = Item.new('2 Days Dogsitting')
+        @item5 = Item.new('Forever Stamps')
+        @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+        @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+        @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
     end
 
     describe '#initialize' do
@@ -21,6 +26,39 @@ RSpec.describe Auction do
             @auction.add_item(@item2)
             expect(@auction.items).to eq([@item1, @item2])
             expect(@auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
+        end
+    end
+
+    describe '#unpopular_items' do
+        it 'returns the items with no bids' do
+            @auction.add_item(@item1)
+            @auction.add_item(@item2)
+            @auction.add_item(@item3)
+            @auction.add_item(@item4)
+            @auction.add_item(@item5)
+            @item1.add_bid(@attendee2, 20)
+            @item1.add_bid(@attendee1, 22)
+            @item4.add_bid(@attendee3, 50)
+
+            expect(@auction.unpopular_items).to eq([@item2, @item3, @item5])
+            @item3.add_bid(@attendee2, 15)
+            expect(@auction.unpopular_items).to eq([@item2, @item5])
+        end
+    end
+
+    describe '#potential_revenue' do
+        it 'returns the total possible sale price of the items (sum of highest bids)' do
+            @auction.add_item(@item1)
+            @auction.add_item(@item2)
+            @auction.add_item(@item3)
+            @auction.add_item(@item4)
+            @auction.add_item(@item5)
+            @item1.add_bid(@attendee2, 20)
+            @item1.add_bid(@attendee1, 22)
+            @item4.add_bid(@attendee3, 50)
+            @item3.add_bid(@attendee2, 15)
+
+            expect(@auction.potential_revenue).to eq(87)
         end
     end
 end
