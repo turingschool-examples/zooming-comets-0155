26dd6fe7c601a6e6c7b2499c627365b68445f754
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-06-11 09:31:17 -0600

    Add tests for Item class It.1

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,2 +1,21 @@
-expect(@item1.name).to eq("Chalkware Piggy Bank")
-#=> "Chalkware Piggy Bank"
+require 'spec/spec_helper'
+
+RSpec.describe do
+  before(:each) do
+    @item1 = Item.new('Chalkware Piggy Bank')
+    #=> #<Item:0x00007fbda98fa1b0 @bids={}, @name="Chalkware Piggy Bank">
+    
+    @item2 = Item.new('Bamboo Picture Frame')
+    #=> #<Item:0x00007fbda91874f0 @bids={}, @name="Bamboo Picture Frame">
+  end
+
+  it "can instantiate" do
+    expect(@item1).to be_an_instance_of(Item)
+    expect(@item2).to be_an_instance_of(Item)
+  end
+
+  it "has a name" do
+    expect(@item1.name).to eq("Chalkware Piggy Bank")
+    #=> "Chalkware Piggy Bank"
+  end
+end
