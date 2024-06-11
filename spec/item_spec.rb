c6e244beb1c696b49f03578c00b34d3f8000236e
Cory Bretsch <corybretsch@gmail.com>
2024-06-11 10:06:29 -0500

    class Item with #name

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,6 +1,4 @@
 require './lib/item'
-require './lib/auction'
-require './lib/attendee'
 
 RSpec.configure do |config|
   config.formatter = :documentation
@@ -11,7 +9,7 @@ RSpec.describe Item do
     it 'exists with attributes' do
       item1 = Item.new('Chalkware Piggy Bank')
 
-      expect(item1).to be_a item
+      expect(item1).to be_a Item
       expect(item1.name).to eq('Chalkware Piggy Bank')
     end
   end
