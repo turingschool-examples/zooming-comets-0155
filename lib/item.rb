fc416a9ac0fbe59791d69c3a849b96682f53574b
Tyler Noble <tmnoble11@gmail.com>
2024-06-11 11:23:10 -0600

    Added: close_bidding method. Added: @biding_open instance variable set to true. Modified: add_bid, adding conditionals reflecting status of allowing bids based on @bidding_open.

--- lib/item.rb
+++ lib/item.rb
@@ -1,17 +1,26 @@
 
 class Item
-    attr_reader :name, :bids
+    attr_reader :name, :bids, :bidding_open
 
     def initialize(name)
         @name = name
         @bids = {}
+        @bidding_open = true
     end
 
     def add_bid(attendee, bid)
-        @bids[attendee] = bid
+        if @bidding_open == true
+            @bids[attendee] = bid
+        else
+            "Bidding is closed for this item."
+        end
     end
 
     def current_high_bid
         @bids.values.max
+    end
+
+    def close_bidding
+        @bidding_open = false        
     end
 end
