16414bf869192cfcb6984cb2e9747daaef4f180b
Cory Bretsch <corybretsch@gmail.com>
2024-06-11 12:55:49 -0500

    last commit

--- lib/attendee.rb
+++ lib/attendee.rb
@@ -1,8 +1,14 @@
 class Attendee 
+  @@all_attendees = []
   attr_reader :name, :budget
   
   def initialize(hash)
     @name = hash[:name]
     @budget = hash[:budget]
+    @@all_attendees << self
+  end
+
+  def self.see_all
+    @@all_attendees
   end
 end
--- lib/auction.rb
+++ lib/auction.rb
@@ -49,4 +49,19 @@ class Auction
     @bidders.uniq
   end
 
+  def bidder_info
+    attendees = Attendee.see_all
+    info_hash = Hash.new(0)
+    attendees.each do |attendee|
+      info_hash[attendee] = Hash.new(0)
+    end
+    info_hash.each do |attendee, sub_hash|
+      sub_hash[:budget] = attendee.budget.delete("$").to_i
+    end
+    info_hash
+    # require 'pry'; binding.pry
+    # info_hash.each do |attendee, sub_hash|
+    #   sub_hash[:items] = @items[0] if attendee.name == @items[0].bids.keys
+    #   require 'pry'; binding.pry      
+  end
 end
