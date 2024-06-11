bef4f428a20462d001d83fb30f7fe41fed385f79
Sid Swaminathan <sidswaminathan@yahoo.com>
2024-06-11 10:35:48 -0500

    add initialize methods for item, attendee, and auction + testing specs

--- lib/attendee.rb
+++ lib/attendee.rb
@@ -1,2 +1,8 @@
 class Attendee
+    attr_reader :name, :budget
+    
+    def initialize(data)
+        @name = data[:name]
+        @budget = data[:budget].delete('$').to_i
+    end
 end
--- lib/auction.rb
+++ lib/auction.rb
@@ -1,2 +1,7 @@
 class Auction
+    attr_reader :items
+
+    def initialize
+        @items = []
+    end
 end
