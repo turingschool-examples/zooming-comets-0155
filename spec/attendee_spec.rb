2ce78ac7f342c482f4abdae0c903439ebb67365d
Cory Bretsch <corybretsch@gmail.com>
2024-06-11 10:09:19 -0500

    attendee test for #initialize with attributes

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -1,3 +1,21 @@
 require './lib/item'
-require './lib/auction'
 require './lib/attendee'
+
+RSpec.configure do |config|
+  config.formatter = :documentation
+end
+
+RSpec.describe Attendee do
+  describe '#initialize' do
+    it 'exists with attributes' do
+      item1 = Item.new('Chalkware Piggy Bank')
+      item2 = Item.new('Bamboo Picture Frame')
+      attendee = Attendee.new({name: 'Megan', budget: '$50'})
+
+      expect(attendee).to be_a Attendee 
+      expect(attendee.name).to eq('Megan')
+      expect(attendee.budget).to eq('$50')
+    end
+  end
+
+end
