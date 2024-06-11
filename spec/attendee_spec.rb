31b0cfc156b635b67d49babf6a39cd0fa64597f9
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-06-11 09:34:39 -0600

    Add tests for Attendee class for It.1

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -1,5 +1,23 @@
-@attendee.name
-#=> "Megan"
+require './spec/spec_helper'
 
-@attendee.budget
-#=> 50
+RSpec.describe do
+  before(:each) do
+    @attendee = Attendee.new({name: 'Megan', budget: '$50'})
+    #=> #<Attendee:0x00007fbda913f038 @budget=50, @name="Megan">
+  end
+
+  it "can instantiate" do
+    expect(@attendee).to be_an_instance_of(Attendee)
+  end
+
+  it "has a name" do
+    expect(@attendee.name).to eq("Megan")
+    #=> "Megan"
+  end
+
+  it "has a budget" do
+    expect(@attendee.budget).to eq(50)
+    #=> 50
+  end
+end
+
