d8c5afd1e80f453c111105136588b958d6c13f47
Tyler Noble <tmnoble11@gmail.com>
2024-06-11 10:01:25 -0600

    Refactored: auction_spec.rb tests, added before each do

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -1,41 +1,44 @@
 require 'spec_helper'
 
 RSpec.describe Auction do
+    before(:each) do
+        @item1 = Item.new('Chalkware Piggy Bank')
+        @item2 = Item.new('Bamboo Picture Frame')
+        @item3 = Item.new('Homemade Chocolate Chip Cookies')
+        @item4 = Item.new('2 Days Dogsitting')
+        @item5 = Item.new('Forever Stamps')
+
+        @auction = Auction.new
+
+        @attendee1 = Attendee.new(name: 'Megan', budget: '$50')
+        @attendee2 = Attendee.new(name: 'Bob', budget: '$75')
+        @attendee3 = Attendee.new(name: 'Mike', budget: '$100')
+    end
+    
     it 'exists' do
-        auction = Auction.new
-        expect(auction).to be_a(Auction)
+        expect(@auction).to be_a(Auction)
     end
 
     it 'has an item' do
-        item1 = Item.new('Chalkware Piggy Bank')
-        auction = Auction.new
+        expect(@auction.item).to eq([])
+        expect(@auction.item).to be_a(Array)
 
-        expect(auction.item).to be_a(Array)
+        @auction.add_item(@item1)
 
-        auction.add_item(item1)
-
-        expect(auction.item).to eq([item1])
+        expect(@auction.item).to eq([@item1])
     end
 
     it 'can add multiple items' do
-        item1 = Item.new('Chalkware Piggy Bank')
-        item2 = Item.new('Bamboo Picture Frame')
-        auction = Auction.new
+        @auction.add_item(@item1)
+        @auction.add_item(@item2)
 
-        auction.add_item(item1)
-        auction.add_item(item2)
-
-        expect(auction.item).to eq([item1, item2])
+        expect(@auction.item).to eq([@item1, @item2])
     end
 
     it 'can return item names' do
-        item1 = Item.new('Chalkware Piggy Bank')
-        item2 = Item.new('Bamboo Picture Frame')
-        auction = Auction.new
+        @auction.add_item(@item1)
+        @auction.add_item(@item2)
 
-        auction.add_item(item1)
-        auction.add_item(item2)
-
-        expect(auction.item_names).to eq(['Chalkware Piggy Bank', 'Bamboo Picture Frame'])
+        expect(@auction.item_names).to eq(['Chalkware Piggy Bank', 'Bamboo Picture Frame'])
     end
 end
