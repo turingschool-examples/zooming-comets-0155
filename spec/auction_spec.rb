4a3a2f01fff17aaaf2a232ed797d7717a2ad8756
Cory Bretsch <corybretsch@gmail.com>
2024-06-11 12:12:30 -0500

    Auction #bidder_info test

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -128,8 +128,34 @@ RSpec.describe Auction do
     end
   end
 
-  # describe '#bidder_info' do
-  #   it 'can return hash with key as attendee object and value of hash with key as :budget and value of budget as integer
-  # end
+  describe '#bidder_info' do
+    it 'can return bidder info according to README example' do
+      item1 = Item.new('Chalkware Piggy Bank')
+      item2 = Item.new('Bamboo Picture Frame')
+      item3 = Item.new('Homemade Chocolate Chip Cookies')
+      item4 = Item.new('2 Days Dogsitting')
+      item5 = Item.new('Forever Stamps')
+      attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+      attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+      attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+      auction = Auction.new
+
+      auction.add_item(item1)
+      auction.add_item(item2)
+      auction.add_item(item3)
+      auction.add_item(item4)
+      auction.add_item(item5)
+      item1.add_bid(attendee2, 20)
+      item1.add_bid(attendee1, 22)
+      item4.add_bid(attendee3, 50)
+      item3.add_bid(attendee2, 15)
+
+      expect(auction.bidder_info).to eq({
+        attendee1 => {:budget => 50, :items => [item1]},
+        attendee2 => {:budget => 75, :items => [item1, item3]},
+        attendee3 => {:budget => 100,:items => [item4]}
+      })
+    end
+  end
 end
 
